6ad0769023ccf8b70a9227a2933e1fd7e11932f7
David Nanjila <nanjiladavid2@gmail.com>
2024-06-17 14:47:21 +0300

    Feat: add  sos button to the landing page

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,15 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import location
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
-=======
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
->>>>>>> 340a50e4ea1bf61e02e56662556e6c52b684151c
 }
