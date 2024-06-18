5d3d6ed855a68031099591d794d2595a54029d50
Teresiah-cpu <terrywambo2018@gmail.com>
2024-06-18 11:28:03 +0300

    fix dependecies issues

--- lib/events.dart
+++ lib/events.dart
@@ -194,8 +194,8 @@ class EventCard extends StatelessWidget {
                     },
                     child: Text('RSVP'),
                     style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.purple,
+                      onPrimary: Colors.white,
+                      primary: Colors.purple,
                     ),
                   ),
                   IconButton(
@@ -274,8 +274,8 @@ class EventDetailsPage extends StatelessWidget {
               },
               child: Text('Add to Calendar'),
               style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.purple,
+                onPrimary: Colors.white,
+                primary: Colors.purple,
               ),
             ),
             SizedBox(height: 20),
